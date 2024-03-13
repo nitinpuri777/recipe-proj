9c7dbf2662937c61c0e70b208ea3b7821c30d004
Nitin Puri <nitinpuri777@gmail.com>
2024-03-13 14:35:57 -0400

    configuring serverless for netlify

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 import express from 'express'
-import ApiSignIn from '../../backend/api/sign-in.js';
-import Middleware from '../../backend/api/_middleware.js';
-import ApiRecipes from '../../backend/api/recipes.js';
+import ApiSignIn from './api/sign-in.js'
+import Middleware from './api/_middleware.js';
+import ApiRecipes from './api/recipes.js';
 const app = express() 
 app.use(express.json())
 app.post('/api/sign-in', ApiSignIn.post)
@@ -9,8 +9,8 @@ app.use('/api', Middleware.authenticateToken)
 app.get('/api/recipes', ApiRecipes.get)
 app.post('/api/recipes', ApiRecipes.post)
 app.delete('/api/recipes/:id', ApiRecipes.delete_)
+app.put('/api/recipes/:id', ApiRecipes.put)
 app.use('/api', Middleware.handleError)
 app.use(express.static('frontend'))
-app.listen(3000, () => {
-    console.info('Server is Ready')
-})
+
+export default app
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,6 @@
-import express from 'express'
-import ApiSignIn from './api/sign-in.js'
-import Middleware from './api/_middleware.js';
-import ApiRecipes from './api/recipes.js';
-const app = express() 
-app.use(express.json())
-app.post('/api/sign-in', ApiSignIn.post)
-app.use('/api', Middleware.authenticateToken)
-app.get('/api/recipes', ApiRecipes.get)
-app.post('/api/recipes', ApiRecipes.post)
-app.delete('/api/recipes/:id', ApiRecipes.delete_)
-app.put('/api/recipes/:id', ApiRecipes.put)
-app.use('/api', Middleware.handleError)
-app.use(express.static('frontend'))
+import app from "./app.js"
+
 app.listen(3000, () => {
     console.info('Server is Ready')
 })
+
