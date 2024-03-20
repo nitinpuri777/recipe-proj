43a5f079c8349745c126df5d0feac1ca6f9a8660
Nitin Puri <nitinpuri777@gmail.com>
2024-03-19 23:25:16 -0400

    Adding step tracking

--- backend/database.js
+++ backend/database.js
@@ -23,32 +23,21 @@ const database = {
           name: "Pizza",
           userId: 1,
           ingredients: ["1000g flour", "200g Cheese"],
-          steps: [
-            {number: 1, description: "Make the dough"},
-            {number: 2, description: "Put the cheese on the pizza"},
-            {number: 3, description: ""}
-          ]
+          steps: ["Make the dough", "Spread the dough out", "Put the sauce on the pizza","Put the cheese on the pizza"]
       },
       {
           id:2,
           name: "Taco",
           userId: 2,
           ingredients: ["4 tortillas", "0.5lb Beef"],
-          steps: [
-            {number: 1, description: "Open the tortilla"},
-            {number: 2, description: "Put the beef in the tortilla"},
-          ]
+          steps: ["Open the tortilla", "Put the beef in the tortilla"]
       },
       {
           id:3,
           name: "Donut",
           userId: 2,
           ingredients: ["1000g flour", "500g Sugar"],
-          steps: [
-            {number: 1, description: "Make the dough"},
-            {number: 2, description: "Fry the dough"},
-            {number: 3, description: "Cover it in sugar"}
-          ]
+          steps: [ "Make the dough","Fry the dough","Cover it in sugar"]
       }
   ]
 
