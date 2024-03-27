e485df18f848398774af7bff793845b77205a355
Nitin Puri <nitinpuri777@gmail.com>
2024-03-26 22:36:51 -0400

    moving recipe box to local component for recipe list

--- frontend/components/recipe-list.js
+++ frontend/components/recipe-list.js
@@ -1,4 +1,25 @@
-import RecipeBox from "./recipe-box.js";
+const RecipeBox = {
+  props: ['recipe'],
+  template: `<div class="row align_center width_150px height_150px border ">
+      <div class="column gap_16 align_center">
+        <div class="row fill ">{{recipe.name}}</div>
+        <div class="row align_center gap_8">
+          <span @click="showEditForm(recipe)" class="recipe_link">edit</span>
+          <span @click="deleteRecipe(recipe)" class="recipe_link">delete</span>
+          <router-link :to="'/recipe/' + recipe.id">view</router-link>
+        </div>
+      </div>
+    </div>`,
+  methods: {
+    deleteRecipe(recipe) {
+      this.$emit("delete-recipe", recipe);
+    },
+    showEditForm(recipe) {
+      this.$emit("show-edit-form", recipe);
+    }
+  }
+}
+
 const RecipeList = {
       props: ['recipes'],
       template: `<main class="row gap_16 pad_16 wrap">
