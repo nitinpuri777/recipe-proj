1c759e84ebfc79f20d66d55119eb636cb044def5
Nitin Puri <nitinpuri777@gmail.com>
2024-04-07 09:58:42 -0400

    formatting header to match body width

--- frontend/components/header.js
+++ frontend/components/header.js
@@ -1,6 +1,8 @@
+import { html } from "../globals.js";
 const Header = {
-  template: 
-  `<div class="row width_fill border_bottom pad_left_16 pad_right_16 pad_top_16 pad_bottom_16 gap_32">
+  template: html`
+  <div class="row width_fill border_bottom border_color_gray align_center_x">
+  <div class="row max_width_1200px width_fill  pad_left_16 pad_right_16 pad_top_16 pad_bottom_16 gap_32">
     <div class="row width_240px shrink_none">
       <a href="/"><img src="/assets/logo.png" class="width_fill"></a>
     </div>
@@ -9,7 +11,8 @@ const Header = {
       <a @click="signOut" class="pad_8 button rounded border">Sign Out</a>
       </div>
     </div>
-  </div>`,
+  </div>
+</div>`,
   methods: {
     signOut: function () {
       this.$emit("sign-out");
