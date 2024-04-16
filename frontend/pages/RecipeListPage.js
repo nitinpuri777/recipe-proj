894160220ae7a87d621d34ec84a5fcaaca0dfe23
Nitin Puri <nitinpuri777@gmail.com>
2024-04-15 20:21:37 -0400

    add on commit

--- frontend/pages/RecipeListPage.js
+++ frontend/pages/RecipeListPage.js
@@ -6,9 +6,11 @@ import UrlScraper from '../components/url-scraper.js'
 const RecipeListPage = {
   template: `
     <app-header />
-    <div class="column pad_16 gap_16 max_width_1200px width_fill">
-    <url-scraper/>
-    <recipe-list class="height_fill"/>
+    <div class="column pad_16 gap_32 max_width_1200px width_fill height_fill">
+    <div class="row width_fill align_center_x">
+     <url-scraper class="pad_top_16 pad_bottom_16 max_width_800px" />
+    </div>
+     <recipe-list />
     </div>
     <right-overlay />
 
