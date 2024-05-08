ffd65ad08cada492bb586147eec2a3dbda3944b9
Nitin Puri <nitinpuri777@gmail.com>
2024-05-08 15:59:10 -0400

    push shopping list changes

--- frontend/pages/ShoppingListPage.js
+++ frontend/pages/ShoppingListPage.js
@@ -1,5 +1,7 @@
 import AppHeader from "../components/app-header.js"
 import { html } from "../globals.js"
+import { parseIngredient } from "parse-ingredient"
+import { toDecimal, toFraction } from 'fraction-parser';
 
 const ShoppingListPage = {
   template: html`
@@ -12,10 +14,14 @@ const ShoppingListPage = {
         </div>
         <div  class="button__secondary" @click="this.$store.createList"> + Create New List </div>
     </div> 
-    <div class ="column max_width_700px gap_8 width_fill">
+    <div class ="column max_width_700px gap_16 width_fill">
       <div class="font_24 pad_left_16 pad_top_32">Shopping List</div>
-      <div class="column bg_gray gap_2 pad_left_8 pad_right_8 pad_top_16 pad_bottom_16"> 
-          <div v-for="item, index in this.$store.currentListItems" class="row align_center_y width_fill gap_fill pad_8 rounded bg_white">
+      <div class="row width_fill pad_left_8 pad_right_8">
+        <input type="text" class="row width_fill pad_8 pad_left_32 rounded border border_color_gray add_item_input"  v-model="inputItem" @keypress.enter="addListItem" placeholder="Add Item"> 
+      </div>
+      <div class="column bg_gray gap_2 pad_left_8 pad_right_8 pad_top_16 pad_bottom_16">
+        <template v-for="item, index in this.$store.currentListItems" :key="index">
+          <div v-if="!this.checkedItems[index]" class="row align_center_y width_fill gap_fill pad_8 rounded bg_white">
             <div class= "row gap_8 align_center_y" :class="{'strikethrough font_color_gray' : checkedItems[index]}">
               <div class="font_16">{{item.ingredientName}}</div>
               <div class="font_12 font_color_gray">{{item.quantity}} {{item.unitOfMeasure}}</div>
@@ -24,6 +30,19 @@ const ShoppingListPage = {
               <input type="checkbox" v-model="checkedItems[index]" class="checkbox" @change="toggleCheckedItem(index)">
             </div>
           </div> 
+        </template>
+        <div v-if="this.checkedItems.some(value => value === true)" class="row pad_top_16 pad_bottom_8 font_bold">Checked Items</div>
+        <template v-for="item, index in this.$store.currentListItems" :key="index">
+          <div v-if="this.checkedItems[index]" class="row align_center_y width_fill gap_fill pad_8 rounded bg_white">
+            <div class= "row gap_8 align_center_y" :class="{'strikethrough font_color_gray' : checkedItems[index]}">
+              <div class="font_16">{{item.ingredientName}}</div>
+              <div class="font_12 font_color_gray">{{item.quantity}} {{item.unitOfMeasure}}</div>
+            </div>
+            <div class="row align_right">
+              <input type="checkbox" v-model="checkedItems[index]" class="checkbox" @change="toggleCheckedItem(index)">
+            </div>
+          </div> 
+        </template>
         </div> 
         <div class="row">
           <input type="text" v-model="inputIngredientName" placeholder="Ingredient Name">
@@ -47,21 +66,24 @@ const ShoppingListPage = {
       inputIngredientName:"",
       inputQuantity:null,
       inputUnitOfMeasure:"",
+      inputItem:"",
       checkedItems: [],
     }
   },
   methods: {
     async addListItem() {
+      let json = parseIngredient(this.inputItem)[0]
+      console.log(json)
       let itemDetails = {
-        ingredientName: this.inputIngredientName,
-        quantity: this.inputQuantity,
-        unitOfMeasure: this.inputUnitOfMeasure
+        ingredientName: json.description,
+        quantity: json.quantity,
+        unitOfMeasure: json.unitOfMeasure
       }
+      this.$store.currentListItems.push(itemDetails)
+      this.inputItem = ""
       await this.$store.createListItem(this.$store.currentListId, itemDetails)
-      this.inputIngredientName = ""
-      this.inputQuantity = ""
-      this.inputUnitOfMeasure = ""
-    },
+      
+    }
   },
   components: {
     AppHeader
