10353e048ac089857c1302deb89af2346c93396e
Nitin Puri <nitinpuri777@gmail.com>
2024-12-25 00:33:02 -0500

    Got Meal Plan Modal somewhat formatted with rounded checkboxes

--- frontend/pages/MealPlanPage.js
+++ frontend/pages/MealPlanPage.js
@@ -1,6 +1,9 @@
 import { ref, onMounted } from 'vue';
 import { useStore } from '../store.js';
 import AppHeader from "../components/app-header.js"
+import GenericModal from "../components/generic-modal.js"
+import RecipeList from "../components/recipe-list.js"
+import SearchHeader from "../components/search-header.js"
 const MealPlanPage = {
   template: `
     <app-header />
@@ -40,20 +43,41 @@ const MealPlanPage = {
                 <div class="row align_left font_20 font_bold"> Saturday </div>
                 <img class="row align_right font_20 font_bold icon" height="24" width="24" src="../assets/icons/plus.svg" />
             </div>
-
         </div>
     </div>
+    <generic-modal :showModal="isRecipeModalVisible" @close="isRecipeModalVisible = false" @confirm="addRecipeToMealPlan(selectedRecipe)" title="Add Recipe" confirmButtonText="Add Recipe">
+      <div class="column width_fill min_width_400px pad_left_8 pad_right_8">
+        <!-- <input type="text" class="row width_fill pad_8 pad_left_36 rounded_20px border border_color_gray search_input" placeholder="Search recipes">  -->
+        <div class="column width_fill gap_16">
+            <div v-for="(recipe, index) in recipes.slice(0,4)" :key="recipe.id" class="row width_fill align_center_y gap_16">
+                <div class="round pad_right_8">
+                    <input type="checkbox" :id="'checkbox-' + index" v-model="recipe.checked" />
+                    <label :for="'checkbox-' + index"></label>
+                </div>
+                <img :src="recipe.image_url" class="row width_fill height_140px border_invisible rounded crop_center"> 
+                <div class="row height_fill align_center_y font_bold pad_8">
+              {{recipe.name}}
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </generic-modal>
   `,
   async mounted() {
-    this.recipes = await this.store.fetchRecipes();
+    this.recipes = await this.$store.fetchRecipes();
   },
   data() {
     return {
         recipes: [],
+        isRecipeModalVisible: true,
     };
   },
   components: {
-    AppHeader
+    AppHeader,
+    GenericModal,
+    RecipeList,
+    SearchHeader
   },
   methods: {
     async submitMealPlan() {
