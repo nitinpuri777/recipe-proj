64acded277c7c451f55340a589f9e8ee492bf21a
Nitin Puri <nitinpuri777@gmail.com>
2025-12-23 13:31:06 -0500

    add recipe to Meal Plan from Recipe Detail

--- frontend/components/recipe-detail.js
+++ frontend/components/recipe-detail.js
@@ -16,6 +16,50 @@ const RecipeDetail = {
           {{ toastMessage }}
         </div>
       </transition>
+
+      <!-- Week Selector Modal -->
+      <div class="modal row align_center width_fill height_fill position_fixed" :class="modalClasses">
+        <!-- modal backdrop -->
+        <div @click="cancelDateSelection" class="modal_overlay__backdrop"></div>
+        <!-- modal content -->
+        <div class="modal_overlay column bg_white border pad_16 gap_16 position_relative rounded_8px border_color_gray week-selector-width week-selector-height" :class="modalOverlayClasses" @click.stop>
+          <!-- Week Navigation -->
+          <div class="row width_fill gap_fill pad_bottom_16">
+            <img class="row align_left align_center_y icon" height="24" width="24" src="/assets/icons/chevron-left.svg" @click="decrementWeek" />
+            <div class="row align_center_x align_center_y font_20 font_bold">
+              {{ weekStart.toLocaleDateString('en-US', { month: 'short', day: 'numeric' }) }} - {{ weekEnd.toLocaleDateString('en-US', { month: 'short', day: 'numeric' }) }}
+            </div>
+            <img class="row align_right align_center_y icon" height="24" width="24" src="/assets/icons/chevron-right.svg" @click="incrementWeek" />
+          </div>
+
+          <!-- Scrollable Days Container -->
+          <div class="week-selector-scroll">
+            <!-- Days of Week -->
+            <div v-for="day in daysOfWeekWithMeals" :key="day.name" class="column width_fill">
+              <div class="row width_fill gap_fill font_20 font_bold pad_top_16 pad_bottom_16 border_top border_color_gray">
+                <div class="row align_left font_20 font_bold">{{ day.name }}</div>
+                <img class="row align_right font_20 font_bold icon" height="24" width="24" src="/assets/icons/plus.svg" @click="selectDay(day.date)" />
+              </div>
+              <div class="row gap_8 pad_bottom_8 wrap">
+                <div v-for="meal in day.meals" :key="meal.meal_plan_id" class="border_invisible rounded recipe_tile meal-tile-small">
+                  <div class="column height_fill width_fill align_top">
+                    <img v-if="meal.recipe && meal.recipe.image_url" :src="meal.recipe.image_url" class="border_invisible rounded crop_center">
+                    <div class="font_bold">
+                      {{ meal.recipe ? meal.recipe.name : 'Unnamed Recipe' }}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Done Button -->
+          <div class="row width_fill pad_top_16 border_top border_color_gray align_right">
+            <button @click="cancelDateSelection" class="button rounded">Done</button>
+          </div>
+        </div>
+      </div>
+
       <div class="row width_fill gap_fill">
         <div class="row align_left">
           <img src="/assets/icons/arrow-left.svg" class="height_28px width_28px icon" @click="goBack">
@@ -69,10 +113,7 @@ const RecipeDetail = {
             </div> 
             <div class="row width_fill align_right position_relative gap_16">
               <template v-if="hasRecipeId">
-                <label style="position: relative; display: inline-block; cursor: pointer;">
-                  <img src="/assets/icons/calendar.svg" class="icon pointer" title="Add to Meal Plan" style="pointer-events: none;">
-                  <input ref="datePicker" type="date" @change="addToMealPlan" style="position: absolute; top: 0; left: 0; width: 28px; height: 28px; opacity: 0; cursor: pointer;">
-                </label>
+                <img @click="openDatePickerModal" src="/assets/icons/calendar.svg" class="icon pointer" title="Add to Meal Plan">
                 <img @click.stop="showModal" src="/assets/icons/shopping-cart.svg" class="icon" title="Add to Shopping List">
               </template>
             </div>
@@ -112,11 +153,48 @@ const RecipeDetail = {
       desiredServings: 0,
       saving: false,
       showToast: false,
-      toastMessage: ''
+      toastMessage: '',
+      showDatePickerModal: false,
+      weekStart: new Date(),
+      weekEnd: new Date(),
+      mealPlans: []
     }
   },
   computed: {
-    derivedScaleFactor() { 
+    modalClasses() {
+      return this.showDatePickerModal ? "" : "modal--hidden";
+    },
+    modalOverlayClasses() {
+      return this.showDatePickerModal ? "" : "modal_overlay--hidden";
+    },
+    daysOfWeek() {
+      const days = [];
+      const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+      for (let i = 0; i < 7; i++) {
+        const date = new Date(this.weekStart);
+        date.setDate(this.weekStart.getDate() + i);
+        days.push({
+          name: dayNames[i],
+          date: date
+        });
+      }
+      return days;
+    },
+    daysOfWeekWithMeals() {
+      return this.daysOfWeek.map(day => {
+        const meals = this.mealPlans ? this.mealPlans.filter(meal => {
+          return meal.meal_date === day.date.toISOString().split('T')[0];
+        }).map(meal => {
+          return {
+            meal_plan_id: meal.meal_plan_id,
+            recipe: meal.recipe
+          };
+        }) : [];
+        return { ...day, meals };
+      });
+    },
+    derivedScaleFactor() {
       if(this.recipeToView.serving_size) {
         console.log(this.recipeToView.serving_size)
         return this.desiredServings / this.recipeToView.serving_size
@@ -239,26 +317,79 @@ const RecipeDetail = {
     showModal() {
       this.$emit('show-modal', this.scaledIngredients)
     },
-    async addToMealPlan(event) {
-      const selectedDate = event.target.value;
-      if (!selectedDate) return;
+    async openDatePickerModal() {
+      this.setWeekStartAndEnd();
+      const response = await this.$store.fetchMealPlans();
+      this.mealPlans = response.mealPlans || [];
+      this.showDatePickerModal = true;
+    },
+    setWeekStartAndEnd() {
+      const today = new Date();
+      const dayOfWeek = today.getDay();
 
+      const start = new Date(today);
+      start.setDate(today.getDate() - dayOfWeek);
+      start.setHours(0, 0, 0, 0);
+
+      const end = new Date(start);
+      end.setDate(start.getDate() + 6);
+      end.setHours(23, 59, 59, 999);
+
+      this.weekStart = start;
+      this.weekEnd = end;
+    },
+    incrementWeek() {
+      const newStart = new Date(this.weekStart);
+      newStart.setDate(this.weekStart.getDate() + 7);
+
+      const newEnd = new Date(this.weekEnd);
+      newEnd.setDate(this.weekEnd.getDate() + 7);
+
+      this.weekStart = newStart;
+      this.weekEnd = newEnd;
+    },
+    decrementWeek() {
+      const newStart = new Date(this.weekStart);
+      newStart.setDate(this.weekStart.getDate() - 7);
+
+      const newEnd = new Date(this.weekEnd);
+      newEnd.setDate(this.weekEnd.getDate() - 7);
+
+      this.weekStart = newStart;
+      this.weekEnd = newEnd;
+    },
+    async selectDay(date) {
       try {
         const mealPlanData = {
           recipe_id: this.recipeToView.id,
-          meal_date: selectedDate
+          meal_date: date.toISOString().split('T')[0]
         };
 
-        await this.$store.createMealPlan(mealPlanData);
+        // Optimistically add to local state
+        const tempMealPlan = {
+          meal_plan_id: `temp-${Date.now()}`,
+          meal_date: date.toISOString().split('T')[0],
+          recipe: this.recipeToView
+        };
+        this.mealPlans.push(tempMealPlan);
 
-        // Reset the date picker
-        this.$refs.datePicker.value = '';
+        const createdMealPlan = await this.$store.createMealPlan(mealPlanData);
 
-        this.displayToast('Recipe added to meal plan!');
+        // Replace temp with real meal plan
+        this.mealPlans = this.mealPlans.map(mp =>
+          mp.meal_plan_id === tempMealPlan.meal_plan_id
+            ? { ...mp, meal_plan_id: createdMealPlan.mealPlan.meal_plan_id }
+            : mp
+        );
       } catch (error) {
         console.error('Error adding recipe to meal plan:', error);
-        this.displayToast('Failed to add recipe to meal plan.');
+        // Refresh meal plans on error
+        const response = await this.$store.fetchMealPlans();
+        this.mealPlans = response.mealPlans || [];
       }
+    },
+    cancelDateSelection() {
+      this.showDatePickerModal = false;
     },
     displayToast(message) {
       this.toastMessage = message;
